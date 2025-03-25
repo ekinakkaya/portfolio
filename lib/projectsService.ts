65d8183c389874112fdfeff5018680a02a08f58e
Ekin Akkaya <ekoeko0654@gmail.com>
2025-03-25 14:20:06 +0300

    More persistent project data logic

--- lib/projectsService.ts
+++ lib/projectsService.ts
@@ -1,4 +1,4 @@
-import { ProjectData, ProjectDataInterface } from "@/types/ProjectData";
+import { ProjectData } from "@/types/ProjectData";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "./firebase";
 
@@ -7,8 +7,8 @@ const DB_DOC_NAME = "projects";
 export const fetchProjects = async (): Promise<ProjectData[]> => {
   const allDocumentsQuerySnapshot = await getDocs(collection(db, DB_DOC_NAME));
   const projects: ProjectData[] = allDocumentsQuerySnapshot.docs.map((doc) => {
-    console.log(doc.data());
-    return new ProjectData(doc.data() as ProjectDataInterface);
+    // return new ProjectData(doc.data() as ProjectDataInterface);
+    return ProjectData.fromFirestore(doc)
   });
 
   return projects;
--- types/ProjectData.ts
+++ types/ProjectData.ts
@@ -1,3 +1,5 @@
+import { QueryDocumentSnapshot, SnapshotOptions } from "firebase/firestore";
+
 export interface ProjectDataInterface {
   id: number;
   title: string;
@@ -8,7 +10,6 @@ export interface ProjectDataInterface {
 }
 
 export class ProjectData implements ProjectDataInterface {
-
   id: number;
   title: string;
   description: string;
@@ -16,29 +17,28 @@ export class ProjectData implements ProjectDataInterface {
   image: string;
   link: string;
 
-  constructor(p: ProjectDataInterface) {
-    this.id = p.id;
-    this.title = p.title;
-    this.description = p.description;
-    this.markdown = p.markdown;
-    this.image = p.image;
-    this.link = p.link;
+  constructor(p: Partial<ProjectDataInterface>) {
+    this.id = p.id ?? 0;
+    this.title = p.title ?? "";
+    this.description = p.description ?? "";
+    this.markdown = p.markdown ?? "";
+    this.image = p.image ?? "";
+    this.link = p.link ?? "";
   }
 
-    toFirestore () {
-      return {
-        id: this.id,
-        title: this.title,
-        description: this.description,
-        markdown: this.markdown,
-        image: this.image,
-        link: this.link,
-      }
-    }
+  toFirestore() {
+    return {
+      id: this.id,
+      title: this.title,
+      description: this.description,
+      markdown: this.markdown,
+      image: this.image,
+      link: this.link,
+    };
+  }
 
-    fromFirestore (snapshot: any, options: any) {
-        const data = snapshot.data(options);
-        return new ProjectData(data);
-    }
-
+  static fromFirestore(snapshot: QueryDocumentSnapshot, options?: SnapshotOptions) {
+    const data = snapshot.data(options) as ProjectDataInterface;
+    return new ProjectData(data);
+  }
 }
